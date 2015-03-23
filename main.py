d1bc7b1d84a4711b8454ce2038ace785797868d8
Vitor Leal <vitorleal1@gmail.com>
2015-03-23 12:04:33 -0300

    add argparse to the main file

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from migration import InstaMigration
-
+import argparse
 
 def main():
-    insta = InstaMigration('YOUR_INSTAGRAM_USER_NAME')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', dest='username', type=str, required=True, help='Your Instagram user name')
+    args = parser.parse_args()
+
+    insta = InstaMigration(args.username)
     insta.make_migration()
 
 
